77b72464985cf246cb863589c8ea51af0d5602d8
Niyaspa <niyaspa8342@gmail.com>
2025-01-11 17:39:50 +0530

    Update disjoint.c

--- disjoint.c
+++ disjoint.c
@@ -1,88 +1,141 @@
 #include <stdio.h>
-void insert_elements(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        printf("Enter element %d: ", i + 1);
-        scanf("%d", &arr[i]);
-    }
+#include <stdlib.h>
+
+struct node {
+    struct node *rep;
+    struct node *next;
+    int data;
+    int rank; 
+};
+
+struct node *heads[50], *tails[50];
+int countRoot = 0;
+
+void makeSet(int x) {
+    struct node *new = (struct node *)malloc(sizeof(struct node));
+    new->rep = new;
+    new->next = NULL;
+    new->data = x;
+    new->rank = 0; 
+    heads[countRoot] = new;
+    tails[countRoot++] = new;
 }
 
-void sort_array(int arr[], int size) {
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = 0; j < size - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+struct node *find(int a) {
+    int i;
+    for (i = 0; i < countRoot; i++) {
+        struct node *tmp = heads[i];
+        while (tmp != NULL) {
+            if (tmp->data == a) {
+                return tmp->rep;
             }
+            tmp = tmp->next;
         }
-    }  
-    
+    }
+    return NULL;
 }
 
-void merge_arrays(int arr1[], int size1, int arr2[], int size2, int merged[]) {
-    int i = 0, j = 0, k = 0;
+void unionSets(int a, int b) {
+    struct node *rep1 = find(a);
+    struct node *rep2 = find(b);
 
-    while (i < size1 && j < size2) {
-        if (arr1[i] < arr2[j]) {
-            merged[k++] = arr1[i++];
+    if (rep1 == NULL || rep2 == NULL) {
+        printf("\nElement not present in the DS\n");
+        return;
+    }
+
+    if (rep1 != rep2) {
+        if (rep1->rank > rep2->rank) {
+            rep2->rep = rep1;
+            struct node *tmp = rep2;
+            while (tmp != NULL) {
+                tmp->rep = rep1;
+                tmp = tmp->next;
+            }
+        } else if (rep1->rank < rep2->rank) {
+            rep1->rep = rep2;
+            struct node *tmp = rep1;
+            while (tmp != NULL) {
+                tmp->rep = rep2;
+                tmp = tmp->next;
+            }
         } else {
-            merged[k++] = arr2[j++];
+            rep2->rep = rep1;
+            struct node *tmp = rep2;
+            while (tmp != NULL) {
+                tmp->rep = rep1;
+                tmp = tmp->next;
+            }
+            rep1->rank++;
         }
-    }
-
-    while (i < size1) {
-        merged[k++] = arr1[i++];
-    }
-    while (j < size2) {
-        merged[k++] = arr2[j++];
     }
 }
 
-int main() {
-    int size1, size2;
-    printf("Enter the size of the first array: ");
-    scanf("%d", &size1);
-    
-    int array1[size1];
-    insert_elements(array1, size1);
-    printf("The unsorted 1st array is:");
-    for(int i = 0 ; i < size1 ; i++) {
-    	printf(" %d ", array1[i]);
-    	}
-    sort_array(array1, size1);
-    printf("\nThe sorted array is:");
-    for(int i = 0 ; i < size1 ; i++) {
-    	printf(" %d ", array1[i]);
-    	}
-    printf("\nEnter the size of the second array: ");
-    scanf("%d", &size2);
-    
-    int array2[size2];
-    insert_elements(array2, size2);
-    printf("The unsorted 2nd array is:");
-    for(int i = 0 ; i < size2 ; i++) {
-    	printf(" %d ",array2[i]);
-    	}
-    sort_array(array2, size2);
-    printf("\nThe sorted array is:");
-    for(int i = 0 ; i < size1 ; i++) {
-    	printf(" %d ", array2[i]);
-    	}
-    
-    int merged[size1 + size2];
-    merge_arrays(array1, size1, array2, size2, merged);
-    sort_array(merged, size1 + size2);
-    printf("\nSorted merged array: ");
-    for (int i = 0; i < size1 + size2; i++) {
-        printf("%d ", merged[i]);
+int search(int x) {
+    int i;
+    for (i = 0; i < countRoot; i++) {
+        struct node *tmp = heads[i];
+        if (tmp->data == x) {
+            return 1;
+        }
+        while (tmp != NULL) {
+            if (tmp->data == x) {
+                return 1;
+            }
+            tmp = tmp->next;
+        }
     }
-    printf("\n");
-
     return 0;
 }
 
+int main() {
+    int choice, x, y;
+    do {
+        printf("\nMENU\n\n1.Make Set\n2.Display set representatives\n3.Union\n4.Find Set\n5.Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
 
+        switch (choice) {
+            case 1:
+                printf("\nEnter new element: ");
+                scanf("%d", &x);
+                if (search(x) == 1) {
+                    printf("\nElement already present in the disjoint set DS\n");
+                } else {
+                    makeSet(x);
+                }
+                break;
+            case 2:
+                printf("\n");
+                for (int i = 0; i < countRoot; i++) {
+                    printf("%d ", heads[i]->data);
+                }
+                printf("\n");
+                break;
+            case 3:
+                printf("\nEnter first element: ");
+                scanf("%d", &x);
+                printf("\nEnter second element: ");
+                scanf("%d", &y);
+                unionSets(x, y);
+                break;
+            case 4:
+                printf("\nEnter the element: ");
+                scanf("%d", &x);
+                struct node *rep = find(x);
+                if (rep == NULL) {
+                    printf("\nElement not present in the DS\n");
+                } else {
+                    printf("\nThe representative of %d is %d\n", x, rep->data);
+                }
+                break;
+            case 5:
+                exit(0);
+            default:
+                printf("\nWrong choice\n");
+                break;
+        }
+    } while (1);
 
-
-
-
+    return 0;
+}
