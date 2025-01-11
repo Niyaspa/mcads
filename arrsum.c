c9de6f78c62e801fbb1631a121c0353534e13215
Niyaspa <niyaspa8342@gmail.com>
2025-01-11 17:19:18 +0530

    Update arrsum.c

--- arrsum.c
+++ arrsum.c
@@ -1,15 +1,20 @@
 #include<stdio.h>
 int main()
 {
-	int ary[10],size,i;
+	int ary[10],elements,i;
 	int sum=0;
 	printf("enter array size\n");
-	scanf("%d",&size);
-	printf("enter array elements\n");
-	for(i=0;i<size;i++)
+	scanf("%d",&elements);
+	for(i=0;i<elements;i++)
+	{
+		printf("enter array elements\n");
 		scanf("%d",&ary[i]);
-	for(i=0;i<size;i++)
+	}
+	for(i=0;i<elements;i++)
+	{
 		sum=sum+ary[i];
+	}
+	
 	printf("sum o f the array=%d\n",sum);
 	return 0;
 }	
